bacb1205f2f29ea2a5b667817362bc282efe6515
Rodion Lezhnyuk <shinigamixrodion@gmail.com>
2023-12-08 23:48:59 +0300

    add few solutions

---
+++ src/main/java/dev/aiko/leetcode/GroupAnagrams.java
@@ -0,0 +1,23 @@
+package dev.aiko.leetcode;
+
+import java.util.*;
+
+// https://leetcode.com/problems/permutations
+public class GroupAnagrams {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    Map<Map<Character, Integer>, List<String>> letterCounterToListOfWords = new HashMap<>();
+    for (String str : strs) {
+      Map<Character, Integer> letterCounter = new HashMap<>();
+      for (int i = 0; i < str.length(); i++) {
+        char symbol = str.charAt(i);
+        int counter = letterCounter.getOrDefault(symbol, 0);
+        letterCounter.put(symbol, counter + 1);
+      }
+      List<String> words =
+          letterCounterToListOfWords.getOrDefault(letterCounter, new ArrayList<>());
+      words.add(str);
+      letterCounterToListOfWords.put(letterCounter, words);
+    }
+    return new ArrayList<>(letterCounterToListOfWords.values());
+  }
+}
