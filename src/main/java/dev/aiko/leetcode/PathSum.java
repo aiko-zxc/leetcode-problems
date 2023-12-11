8653b9e0830d7966befc9c4e8a88cbd7c0160062
Rodion Lezhnyuk <shinigamixrodion@gmail.com>
2023-12-11 23:06:04 +0300

    add solution(s)

--- src/main/java/dev/aiko/leetcode/PathSum.java
+++ src/main/java/dev/aiko/leetcode/PathSum.java
@@ -1,38 +1,22 @@
 package dev.aiko.leetcode;
 
+import dev.aiko.leetcode.common.TreeNode;
+
 // https://leetcode.com/problems/path-sum
 public class PathSum {
 
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        return check(root, targetSum, 0);
+  public boolean hasPathSum(TreeNode root, int targetSum) {
+    return check(root, targetSum, 0);
+  }
+
+  public boolean check(TreeNode root, int targetSum, int currentSum) {
+    if (root == null) {
+      return false;
     }
-
-    public boolean check(TreeNode root, int targetSum, int currentSum) {
-        if (root == null) {
-            return false;
-        }
-        if (root.val + currentSum == targetSum && root.left == null && root.right == null) {
-            return true;
-        }
-        return check(root.left, targetSum, currentSum + root.val)
-                || check(root.right, targetSum, currentSum + root.val);
+    if (root.val + currentSum == targetSum && root.left == null && root.right == null) {
+      return true;
     }
-
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {}
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
+    return check(root.left, targetSum, currentSum + root.val)
+        || check(root.right, targetSum, currentSum + root.val);
+  }
 }
