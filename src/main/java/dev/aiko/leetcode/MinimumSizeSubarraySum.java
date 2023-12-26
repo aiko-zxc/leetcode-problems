c8099dfaa8ec983d3569a6c4147e325324c99298
Rodion Lezhnyuk <shinigamixrodion@gmail.com>
2023-12-26 19:35:37 +0300

    add solution(s)

---
+++ src/main/java/dev/aiko/leetcode/MinimumSizeSubarraySum.java
@@ -0,0 +1,26 @@
+package dev.aiko.leetcode;
+
+// https://leetcode.com/problems/minimum-size-subarray-sum
+public class MinimumSizeSubarraySum {
+  public int minSubArrayLen(int target, int[] nums) {
+    int start = 0;
+    int end = 0;
+    int sum = 0;
+    int result = Integer.MAX_VALUE;
+    for (int i = 0; i < nums.length; i++) {
+      end = i;
+      sum += nums[end];
+      while (sum >= target) {
+        if (end - start < result) {
+          result = end - start;
+        }
+        sum -= nums[start];
+        start++;
+      }
+    }
+    if (result == Integer.MAX_VALUE) {
+      return 0;
+    }
+    return result + 1;
+  }
+}
